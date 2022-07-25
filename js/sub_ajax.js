7f5e1d965138dad48a70f624d6367174e3debbf8
myeongyunChoi <104118233+myeongyunChoi@users.noreply.github.com>
2022-07-25 19:18:19 +0900

    2_sub

--- js/sub_ajax.js
+++ js/sub_ajax.js
@@ -9,19 +9,20 @@ $(function(){
         .done(function (msg) {
 
             $(".book_img").append("<img src='" + msg.documents[1].thumbnail + "'/>");
-
-            // let essay_box = document.getElementsByClassName("essay_box");
-            // for (let i = 0; i < essay_box.length; i++) {
-            //     $(".essay_box").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
-            //     $(".essay_box").eq(i).append("<h3>" + '<a href="">' + msg.documents[i].title + "</a>" + "</h3>");
-            //     $(".essay_box").eq(i).append("<h5>" + msg.documents[i].authors + "</h5>");
-
-
-            //     let str = msg.documents[i].contents;
-            //     let str2 = str.substring(0, 92);
-
-            //     $(".essay_box").eq(i).append("<p>" + '<a href="">' + str2 + "..." + "</a>" + "</p>");
-            //     $(".essay_box").eq(i).append('<a href="#" class="btn">' + "MORE" + "</a>");
-            // }
         });
+
+        $.ajax({
+            method: "GET",
+            url: "https://dapi.kakao.com/v3/search/book?target=title",
+            data: { query: "베르나르베르베르", sort:" accuracy", page: "1" },
+            headers: { Authorization: "KakaoAK 7825d323e9c066c84dbfeebce8829253" }
+        })
+            .done(function (msg) {
+                let series_box = document.getElementsByClassName("series_box");
+                for (let i = 0; i < series_box.length; i++) {
+                    $(".series_box").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
+                    $(".series_box").eq(i).append("<h3>" + '<a href="">' + msg.documents[i].title + "</a>" + "</h3>");
+                    $(".series_box").eq(i).append("<h5>" + msg.documents[i].authors + "</h5>");
+                }
+            });    
 });
